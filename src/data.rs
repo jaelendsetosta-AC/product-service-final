19ca14e154c6851dce03e7d3c99aa54089994a85
jaelendsetosta-AC <seto0046@algonquinlive.com>
2024-12-09 15:16:56 -0500

    Changing front end to tailor best buy

--- src/data.rs
+++ src/data.rs
@@ -5,73 +5,32 @@ pub fn fetch_products(_settings: &Settings) -> Vec<Product> {
     vec![
         Product {
             id: 1,
-            name: "Contoso Catnip's Friend".to_string(),
-            price: 9.99,
-            description: "Watch your feline friend embark on a fishing adventure with Contoso Catnip's Friend toy. Packed with irresistible catnip and dangling fish lure.".to_string(),
-            image: "/catnip.jpg".to_string()
+            name: "Corsair SABRE PRO CHAMPION SERIES Ultra-Light".to_string(),
+            price: 59.99,
+            description: "Experience pinpoint precision with the Corsair SABRE PRO gaming mouse, designed for esports champions with ultra-light construction and hyper-fast response.".to_string(),
+            image: "/mouse.jpg".to_string()
         },
         Product {
             id: 2,
-            name: "Salty Sailor's Squeaky Squid".to_string(),
-            price: 6.99,
-            description: "Let your dog set sail with the Salty Sailor's Squeaky Squid. This interactive toy provides hours of fun, featuring multiple squeakers and crinkle tentacles.".to_string(),
-            image: "/squid.jpg".to_string()
+            name: "K70 CORE SE RGB Mechanical Gaming Keyboard".to_string(),
+            price: 129.55,
+            description: "Take your gaming to the next level with the K70 CORE SE RGB keyboard, featuring customizable backlighting and ultra-responsive mechanical keys.".to_string(),
+            image: "/keyboard.jpg".to_string()
         },
         Product {
             id: 3,
-            name: "Mermaid's Mice Trio".to_string(),
-            price: 12.99,
-            description: "Entertain your kitty with the Mermaid's Mice Trio. These adorable plush mice are dressed as mermaids and filled with catnip to captivate their curiosity.".to_string(),
-            image: "/mermaid.jpg".to_string()
+            name: "VIRTUOSO RGB WIRELESS High-Fidelity Gaming Headset".to_string(),
+            price: 199.99,
+            description: "Immerse yourself in premium sound with the VIRTUOSO RGB WIRELESS headset, featuring high-fidelity audio, RGB lighting, and ultra-comfortable design".to_string(),
+            image: "/headphones.jpg".to_string()
         },
         Product {
             id: 4,
-            name: "Ocean Explorer's Puzzle Ball".to_string(),
-            price: 11.99,
-            description: "Challenge your pet's problem-solving skills with the Ocean Explorer's Puzzle Ball. This interactive toy features hidden compartments and treats, providing mental stimulation and entertainment.".to_string(),
-            image: "/ocean.jpg".to_string()
+            name: "Corsair XENEON Flex 45".to_string(),
+            price: 1499.99,
+            description: "Redefine your gaming visuals with the Corsair XENEON Flex 45, a high-resolution OLED monitor with unparalleled flexibility and vivid colors.".to_string(),
+            image: "/monitor.jpg".to_string()
         },
-        Product {
-            id: 5,
-            name: "Pirate Parrot Teaser Wand".to_string(),
-            price: 8.99,
-            description: "Engage your cat in a playful pursuit with the Pirate Parrot Teaser Wand. The colorful feathers and jingling bells mimic the mischievous charm of a pirate's parrot.".to_string(),
-            image: "/pirate.jpg".to_string()
-        },
-        Product {
-            id: 6,
-            name: "Seafarer's Tug Rope".to_string(),
-            price: 14.99,
-            description: "Tug-of-war meets nautical adventure with the Seafarer's Tug Rope. Made from marine-grade rope, it's perfect for interactive play and promoting dental health in dogs.".to_string(),
-            image: "/tug.jpg".to_string()
-        },
-        Product {
-            id: 7,
-            name: "Seashell Snuggle Bed".to_string(),
-            price: 19.99,
-            description: "Give your furry friend a cozy spot to curl up with the Seashell Snuggle Bed. Shaped like a seashell, this plush bed provides comfort and relaxation for cats and small dogs.".to_string(),
-            image: "/bed.jpg".to_string()
-        },
-        Product {
-            id: 8,
-            name: "Nautical Knot Ball".to_string(),
-            price: 7.99,
-            description: "Unleash your dog's inner sailor with the Nautical Knot Ball. Made from sturdy ropes, it's perfect for fetching, tugging, and satisfying their chewing needs.".to_string(),
-            image: "/knot.jpg".to_string()
-        },
-        Product {
-            id: 9,
-            name: "Contoso Claw's Crabby Cat Toy".to_string(),
-            price: 3.99,
-            description: "Watch your cat go crazy for Contoso Claw's Crabby Cat Toy. This crinkly and catnip-filled toy will awaken their hunting instincts and provide endless entertainment.".to_string(),
-            image: "/crabby.jpg".to_string()
-        },
-        Product {
-            id: 10,
-            name: "Ahoy Doggy Life Jacket".to_string(),
-            price: 5.99,
-            description: "Ensure your furry friend stays safe during water adventures with the Ahoy Doggy Life Jacket. Designed for dogs, this flotation device offers buoyancy and visibility in style.".to_string(),
-            image: "/lifejacket.jpg".to_string()
-        }
+        
     ]
 }
